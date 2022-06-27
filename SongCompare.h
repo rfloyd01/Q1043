5f63e789bca07211b83750a031508f25bec4dbad
rfloyd01 <lynrdskynrd05@gmail.com>
2022-06-27 16:15:18 -0400

    data validated through 2004

---
+++ SongCompare.h
@@ -0,0 +1,126 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+//a function for comparing the text of two excel columns. all data has been searched in Spotify and cleaned up
+//in Excel, this function is to help audit any songs that got mixed up in the process.
+
+void textCompare(std::string fileName) {
+	std::ifstream myFile;
+	myFile.open("Song_Data/" + fileName + ".txt");
+
+	std::vector<std::string> results;
+	std::string currentLine = "";
+	std::string splitLine = "";
+	char delimeter = 9;
+
+	for (int i = 0; i < 1043; i++) {
+		std::getline(myFile, currentLine); //split each line at the tab
+
+		//text file is created from excel and track names are separated with a horizontal tab
+		//(ASCII character 9).
+		//std::cout << currentLine << std::endl;
+		int currentString = 0; //needed to split out period after ranking number
+		std::pair<std::string, std::string> comparison;
+		std::stringstream ss(currentLine);
+		while (std::getline(ss, splitLine, delimeter))
+		{
+			if (!(currentString++)) {
+				//this is the first word, remove the number and period
+				comparison.first = splitLine.substr(splitLine.find('.') + 2);
+			}
+			else comparison.second = splitLine;
+		}
+
+		//check to see if either of our strings has parantheses in them, by removing the contents inside we got a lot more matches
+		//without compromising songs that aren't matches. For example "Rosalita (Come Out Tonight)" and "Rosalita" will now match
+		//after removing parantheses but "Rosalita" and "Jackie Wilson Said (I'm in Heaven When You Smile)" wouldn't.
+		if (comparison.first.find('(') != 4294967295)
+		{
+			//almost every song with parentheses has them at the start or end. It may effect a few songs, but we'll ignore
+			//any songs with parentheses in the middle of the string for simplicity
+			if (comparison.first.find('(') == 0) comparison.first = comparison.first.substr(comparison.first.find(')') + 2);
+			else  comparison.first = comparison.first.substr(0, comparison.first.find('('));
+
+		}
+		if (comparison.second.find('(') != 4294967295)
+		{
+			//almost every song with parentheses has them at the start or end. It may effect a few songs, but we'll ignore
+			//any songs with parentheses in the middle of the string for simplicity
+			if (comparison.second.find('(') == 0) comparison.second = comparison.second.substr(comparison.second.find(')') + 2);
+			else  comparison.second = comparison.second.substr(0, comparison.second.find('('));
+
+		}
+		
+		//with the string properly split we can start the comparison algorithm (this is more or less copy and pasted
+		//from my front end.
+		double score = 0;
+
+		//create arrays to count the number of characters and numbers in each word
+		int oneLetters[26] = { 0 };
+		int twoLetters[26] = { 0 };
+		int oneNumbers[10] = { 0 };
+		int twoNumbers[10] = { 0 };
+
+		//cast both strings to uppercase for an easier comparison
+		for (int j = 0; j < comparison.first.length(); j++)
+		{
+			//convert letters to uppercase if necessary
+			comparison.first[j] = toupper(comparison.first[j]);
+
+			if ((comparison.first[j] >= (int)'0') && (comparison.first[j] <= (int)'9')) oneNumbers[comparison.first[j] - '0']++;
+			else if ((comparison.first[j] >= (int)'A') && (comparison.first[j] <= (int)'Z')) oneLetters[comparison.first[j] - 'A']++;
+		}
+		for (int j = 0; j < comparison.second.length(); j++)
+		{
+			//convert letters to uppercase if necessary
+			comparison.second[j] = toupper(comparison.second[j]);
+
+			if ((comparison.second[j] >= (int)'0') && (comparison.second[j] <= (int)'9')) twoNumbers[comparison.second[j] - '0']++;
+			else if ((comparison.second[j] >= (int)'A') && (comparison.second[j] <= (int)'Z')) twoLetters[comparison.second[j] - 'A']++;
+		}
+
+		int charactersIncluded = 36; //we chip away from this (total possible letters + total possible numbers)
+		double lower = 0, higher = 0;
+
+		//first compare numbers
+		for (int j = 0; j < 10; j++)
+		{
+			if (!(oneNumbers[j] || twoNumbers[j])) charactersIncluded--; //both are zero so don't include
+			else
+			{
+				//at least one of the numbers is non-zero. we take the lower number and divide by the higher
+				lower = (oneNumbers[j] < twoNumbers[j]) ? oneNumbers[j] : twoNumbers[j];
+				higher = (lower == oneNumbers[j]) ? twoNumbers[j] : oneNumbers[j];
+
+				score += (lower / higher);
+			}
+		}
+
+		//reset these variables to 0 before counting letters
+		lower = 0;
+		higher = 0;
+
+		//then compare letters
+		for (int j = 0; j < 26; j++)
+		{
+			if (!(oneLetters[j] || twoLetters[j])) charactersIncluded--; //both are zero so don't include
+			else
+			{
+				//at least one of the numbers is non-zero. we take the lower number and divide by the higher
+				lower = (oneLetters[j] < twoLetters[j]) ? oneLetters[j] : twoLetters[j];
+				higher = (lower == oneLetters[j]) ? twoLetters[j] : oneLetters[j];
+
+				score += (lower / higher);
+			}
+		}
+
+		score /= (double)charactersIncluded;
+
+		//if the score similarity is 75% or over than we can effectively say the songs are the same:
+		if (score < 0.5) std::cout << i + 1 << ": " << comparison.first << std::endl;
+	}
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <chrono>
 #include <string>
+#include "SongCompare.h"
 
 struct Song
 {
@@ -117,161 +118,164 @@ int alphabeticallyFirst(std::string one, std::string two)
 
 int main()
 {
-	//start a timer to see how long this takes
 	auto run_time = std::chrono::steady_clock::now();
-
-	//Used to figure out the appropriate column size for database
-	int longestSongName = 0;
-	int longestArtistName = 0;
-	std::string longestSong = "";
-	std::string longestArtist = "";
-
-	std::ifstream myFile;
-	std::string songString;
-
-	std::vector<Song> allSongs;
-
-	//read through each character...one at a time...for all 1043 songs
-	//long long location = 0; //a long long is overkill here right?
-
-	for (int year = 2021; year >= 2001; year--)
-	{
-		myFile.open("Song_Data/" + std::to_string(year) + ".txt");
-
-		int numberOfSongs = 1043;
-		if (year <= 2003) numberOfSongs = 104; //apparently they only went up to 104 until the year 2004
-
-		bool artistsIncluded = false;
-		if (year == 2021 || year == 2019 || year == 2014) artistsIncluded = true; //for some reason the artists are only included for these years
-
-		for (int i = 1; i <= numberOfSongs; i++)
-		{
-			//songs are either of the form:
-			//#. Song Name 
-			//or
-			//#. Song Name - Artist Name
-			//For years where artists are included I'll scan backwards until hitting the first '-' character,
-			//and consider everything after that to be the artist (not sure what will happen to any artists that happen
-			//to have a hyphen in their name, sorry Ne-Yo!)
-
-			//Read each song one line at a time
-			Song currentSong;
-			std::getline(myFile, songString);
-			int location = 0, end = songString.length() - 1, compareValue = 0;
-
-			if (artistsIncluded)
-			{
-				while (songString[end] != '-') end--;
-
-				for (int j = end + 2; j < songString.length(); j++)
-					if (songString[j] != ',') currentSong.artist += songString[j]; //ignore commas because of comma delimitting for Excel
-				end -= 2; //this puts the end of the song title to the approprite spot right before the '-'
-
-				if (currentSong.artist.length() > longestArtistName)
-				{
-					longestArtistName = currentSong.artist.length();
-					longestArtist = currentSong.artist;
-				}
-			}
-
-			//Iterate through the line until we hit the fist '.' character, the song starts two characters after this
-			while (songString[location] != '.') location++;
-			location += 2;
-
-			//iterate until the end of the song title and capture each character in the title field of currentSong
-			while (location <= end)
-			{
-				//I'm using comma delimitting in Excel so just leave off any commas in song titles or artist names
-				if (songString[location] != ',') currentSong.title += songString[location];
-				location++;
-			}
-			currentSong.yearsNomiated[year - 2001] = true;
-			currentSong.rankByYear[year - 2001] = i;
-
-			if (currentSong.title.length() > longestSongName)
-			{
-				longestSongName = currentSong.title.length();
-				longestSong = currentSong.title;
-			}
-			//For some reason the year 2021 has the songs counting down instead of up (maybe because it's
-			//the most recent). Adjust the song rankings here
-			if (year == 2021) currentSong.rankByYear[year - 2001] = (i + 1044 - 2 * i);
-
-			//Place song in the ongoing list using a binary search algorithm
-			int front = 0, back = allSongs.size() - 1, search_index = 0;
-			while (true)
-			{
-				if (front > back)
-				{
-					allSongs.insert(allSongs.begin() + front, currentSong);
-					break;
-				}
-
-				search_index = (front + back) / 2;
-				compareValue = alphabeticallyFirst(currentSong.title, allSongs[search_index].title);
-
-				if (compareValue == 2)
-				{
-					//The song names are the same, we have to be carfeul though because there are some
-					//songs by different artists that have the same name (i.e. All Along the Watchtower
-					//by Jimi Hendrix and Bob Dylan). Check to see if this song has already been placed this year
-					//and if so, add it as a distinct line item. This will add more line items overall, but,
-					//prevents overwritting of useful data. Generally speaking, it's unlikely that songs with the
-					//same names will be close to eachother on the list (i.e. Jimi Hendrix's All along the Watchtower
-					//will probably always be ranked a few hundred places higher than Bob Dylan's). Since we'll always
-					//encounter duplicates in the same order, we insert duplicate songs AFTER the current song instead
-					//of before it.
-					if (allSongs[search_index].yearsNomiated[year - 2001])
-					{
-						//song has already been added this year, add it as a new line item after the currently
-						//found song
-						auto yote = year;
-						auto yeet = allSongs[search_index];
-
-						allSongs.insert(allSongs.begin() + search_index + 1, currentSong);
-						break;
-					}
-
-					//Song hasn't been added this year already
-					if (artistsIncluded) allSongs[search_index].artist = currentSong.artist;
-					allSongs[search_index].yearsNomiated[year - 2001] = true;
-					allSongs[search_index].rankByYear[year - 2001] = i;
-					break;
-				}
-				else if (compareValue == 1) back = search_index - 1;
-				else front = search_index + 1;
-			}
-		}
-
-		//close the file once we're done with it
-		myFile.close();
-	}
+	//start a timer to see how long this takes
 	
-	/*std::cout << "The longest song name has a length of " << longestSongName << " characters." << std::endl;
-	std::cout <<  longestSong << std::endl;
-	std::cout << "The longest artist name has a length of " << longestArtistName << " characters." << std::endl;
-	std::cout << longestArtist << std::endl;*/
-	//int artist_missing = 0;
 
-	//print the songs in a CSV format to an external file, make sure to leave a space for new title and new artist which 
-	//will help in auditing Spotify's performance
-	std::ofstream outputFile;
-	outputFile.open("Song_Data/combined_data.txt");
-	for (int i = 0; i < allSongs.size(); i++)
-	{
-		//if (allSongs[i].artist == "") artist_missing++;
-		outputFile << (allSongs[i].artist + ",," + allSongs[i].title + ",,");
-		for (int j = 0; j < 20; j++)
-		{
-			if (allSongs[i].yearsNomiated[j]) outputFile << allSongs[i].rankByYear[j] << ",";
-			else outputFile << "0,";
-		}
-		if (allSongs[i].yearsNomiated[20]) outputFile << allSongs[i].rankByYear[20];
-		else outputFile << "0";
-		outputFile << "\n";
-	}
+	////Used to figure out the appropriate column size for database
+	//int longestSongName = 0;
+	//int longestArtistName = 0;
+	//std::string longestSong = "";
+	//std::string longestArtist = "";
 
-	//std::cout << "There are currently " << artist_missing << " lines of data without an artist" << std::endl;
+	//std::ifstream myFile;
+	//std::string songString;
+
+	//std::vector<Song> allSongs;
+
+	////read through each character...one at a time...for all 1043 songs
+	////long long location = 0; //a long long is overkill here right?
+
+	//for (int year = 2021; year >= 2001; year--)
+	//{
+	//	myFile.open("Song_Data/" + std::to_string(year) + ".txt");
+
+	//	int numberOfSongs = 1043;
+	//	if (year <= 2003) numberOfSongs = 104; //apparently they only went up to 104 until the year 2004
+
+	//	bool artistsIncluded = false;
+	//	if (year == 2021 || year == 2019 || year == 2014) artistsIncluded = true; //for some reason the artists are only included for these years
+
+	//	for (int i = 1; i <= numberOfSongs; i++)
+	//	{
+	//		//songs are either of the form:
+	//		//#. Song Name 
+	//		//or
+	//		//#. Song Name - Artist Name
+	//		//For years where artists are included I'll scan backwards until hitting the first '-' character,
+	//		//and consider everything after that to be the artist (not sure what will happen to any artists that happen
+	//		//to have a hyphen in their name, sorry Ne-Yo!)
+
+	//		//Read each song one line at a time
+	//		Song currentSong;
+	//		std::getline(myFile, songString);
+	//		int location = 0, end = songString.length() - 1, compareValue = 0;
+
+	//		if (artistsIncluded)
+	//		{
+	//			while (songString[end] != '-') end--;
+
+	//			for (int j = end + 2; j < songString.length(); j++)
+	//				if (songString[j] != ',') currentSong.artist += songString[j]; //ignore commas because of comma delimitting for Excel
+	//			end -= 2; //this puts the end of the song title to the approprite spot right before the '-'
+
+	//			if (currentSong.artist.length() > longestArtistName)
+	//			{
+	//				longestArtistName = currentSong.artist.length();
+	//				longestArtist = currentSong.artist;
+	//			}
+	//		}
+
+	//		//Iterate through the line until we hit the fist '.' character, the song starts two characters after this
+	//		while (songString[location] != '.') location++;
+	//		location += 2;
+
+	//		//iterate until the end of the song title and capture each character in the title field of currentSong
+	//		while (location <= end)
+	//		{
+	//			//I'm using comma delimitting in Excel so just leave off any commas in song titles or artist names
+	//			if (songString[location] != ',') currentSong.title += songString[location];
+	//			location++;
+	//		}
+	//		currentSong.yearsNomiated[year - 2001] = true;
+	//		currentSong.rankByYear[year - 2001] = i;
+
+	//		if (currentSong.title.length() > longestSongName)
+	//		{
+	//			longestSongName = currentSong.title.length();
+	//			longestSong = currentSong.title;
+	//		}
+	//		//For some reason the year 2021 has the songs counting down instead of up (maybe because it's
+	//		//the most recent). Adjust the song rankings here
+	//		if (year == 2021) currentSong.rankByYear[year - 2001] = (i + 1044 - 2 * i);
+
+	//		//Place song in the ongoing list using a binary search algorithm
+	//		int front = 0, back = allSongs.size() - 1, search_index = 0;
+	//		while (true)
+	//		{
+	//			if (front > back)
+	//			{
+	//				allSongs.insert(allSongs.begin() + front, currentSong);
+	//				break;
+	//			}
+
+	//			search_index = (front + back) / 2;
+	//			compareValue = alphabeticallyFirst(currentSong.title, allSongs[search_index].title);
+
+	//			if (compareValue == 2)
+	//			{
+	//				//The song names are the same, we have to be carfeul though because there are some
+	//				//songs by different artists that have the same name (i.e. All Along the Watchtower
+	//				//by Jimi Hendrix and Bob Dylan). Check to see if this song has already been placed this year
+	//				//and if so, add it as a distinct line item. This will add more line items overall, but,
+	//				//prevents overwritting of useful data. Generally speaking, it's unlikely that songs with the
+	//				//same names will be close to eachother on the list (i.e. Jimi Hendrix's All along the Watchtower
+	//				//will probably always be ranked a few hundred places higher than Bob Dylan's). Since we'll always
+	//				//encounter duplicates in the same order, we insert duplicate songs AFTER the current song instead
+	//				//of before it.
+	//				if (allSongs[search_index].yearsNomiated[year - 2001])
+	//				{
+	//					//song has already been added this year, add it as a new line item after the currently
+	//					//found song
+	//					auto yote = year;
+	//					auto yeet = allSongs[search_index];
+
+	//					allSongs.insert(allSongs.begin() + search_index + 1, currentSong);
+	//					break;
+	//				}
+
+	//				//Song hasn't been added this year already
+	//				if (artistsIncluded) allSongs[search_index].artist = currentSong.artist;
+	//				allSongs[search_index].yearsNomiated[year - 2001] = true;
+	//				allSongs[search_index].rankByYear[year - 2001] = i;
+	//				break;
+	//			}
+	//			else if (compareValue == 1) back = search_index - 1;
+	//			else front = search_index + 1;
+	//		}
+	//	}
+
+	//	//close the file once we're done with it
+	//	myFile.close();
+	//}
+	//
+	///*std::cout << "The longest song name has a length of " << longestSongName << " characters." << std::endl;
+	//std::cout <<  longestSong << std::endl;
+	//std::cout << "The longest artist name has a length of " << longestArtistName << " characters." << std::endl;
+	//std::cout << longestArtist << std::endl;*/
+	////int artist_missing = 0;
+
+	////print the songs in a CSV format to an external file, make sure to leave a space for new title and new artist which 
+	////will help in auditing Spotify's performance
+	//std::ofstream outputFile;
+	//outputFile.open("Song_Data/combined_data.txt");
+	//for (int i = 0; i < allSongs.size(); i++)
+	//{
+	//	//if (allSongs[i].artist == "") artist_missing++;
+	//	outputFile << (allSongs[i].artist + ",," + allSongs[i].title + ",,");
+	//	for (int j = 0; j < 20; j++)
+	//	{
+	//		if (allSongs[i].yearsNomiated[j]) outputFile << allSongs[i].rankByYear[j] << ",";
+	//		else outputFile << "0,";
+	//	}
+	//	if (allSongs[i].yearsNomiated[20]) outputFile << allSongs[i].rankByYear[20];
+	//	else outputFile << "0";
+	//	outputFile << "\n";
+	//}
+
+	////std::cout << "There are currently " << artist_missing << " lines of data without an artist" << std::endl;
+
+textCompare("2004_compare");
 
 	std::cout << "Ran in ";
 	std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - run_time).count() / 1000000000.0;
