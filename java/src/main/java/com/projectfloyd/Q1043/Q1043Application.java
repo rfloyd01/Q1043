c1173692a8aac274593773eca81c7cd7b6c8af56
rfloyd01 <lynrdskynrd05@gmail.com>
2022-07-13 09:17:02 -0400

    enabled https connections on backend

--- java/src/main/java/com/projectfloyd/Q1043/Q1043Application.java
+++ java/src/main/java/com/projectfloyd/Q1043/Q1043Application.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Q1043Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Q1043Application.class, args);
+		SpringApplication application = new SpringApplication(Q1043Application.class);
+		application.setAdditionalProfiles("ssl");
+		application.run(args);
+		//SpringApplication.run(Q1043Application.class, args);
 	}
 
 }
--- java/src/main/java/com/projectfloyd/Q1043/controllers/AlbumController.java
+++ java/src/main/java/com/projectfloyd/Q1043/controllers/AlbumController.java
@@ -5,12 +5,21 @@ import com.projectfloyd.Q1043.models.Song;
 import com.projectfloyd.Q1043.models.Year;
 import com.projectfloyd.Q1043.services.AlbumService;
 import org.apache.coyote.Response;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,11 +35,26 @@ public class AlbumController {
         this.albumService = albumService;
     }
 
+    @Value("${trust.store}")
+    private Resource trustStore;
+
+    @Value("${trust.store.password}")
+    private String trustStorePassword;
+
+    private RestTemplate restTemplate() throws Exception {
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+        HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(factory);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Album> findAlbumById(@PathVariable("id") int id) {
         Album album = albumService.getAlbumById(id);
 
         if (album != null) {
+            //ResponseEntity<Album> response = restTemplate().getForEntity("https://localhost:443/")
             return ResponseEntity.status(HttpStatus.OK).body(album);
         }
         else {
